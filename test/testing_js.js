9d524e52a56b8ea6b710b1f0ce02b0826836b243
info@dgt-international.com <info@dgt-international.com>
2025-08-07 14:05:39 -0400

    running test in javascript

--- test/testing_js.js
+++ test/testing_js.js
@@ -1,26 +1,29 @@
-// Create a Graphology graph
-const graph = new graphology.Graph();
+// Wait until the DOM is fully loaded
+window.addEventListener("DOMContentLoaded", () => {
+  // Create a graphology graph
+  const graph = new graphology.Graph();
 
-graph.addNode("1", {
-  label: "Node 1",
-  x: 0,
-  y: 0,
-  size: 10,
-  color: "blue"
+  graph.addNode("1", {
+    label: "Node 1",
+    x: 0,
+    y: 0,
+    size: 10,
+    color: "blue"
+  });
+
+  graph.addNode("2", {
+    label: "Node 2",
+    x: 1,
+    y: 1,
+    size: 20,
+    color: "red"
+  });
+
+  graph.addEdge("1", "2", {
+    size: 5,
+    color: "purple"
+  });
+
+  // Render with Sigma
+  const renderer = new sigma(graph, document.getElementById("container"));
 });
-
-graph.addNode("2", {
-  label: "Node 2",
-  x: 1,
-  y: 1,
-  size: 20,
-  color: "red"
-});
-
-graph.addEdge("1", "2", {
-  size: 5,
-  color: "purple"
-});
-
-// Create and mount the Sigma renderer
-const renderer = new sigma.Sigma(graph, document.getElementById("container"));
