322dba1531f49309751314f3cbf437d329eaa36b
info@dgt-international.com <info@dgt-international.com>
2025-08-31 00:34:41 -0400

    finalizing edges filters

--- code/R Shiny app.R
+++ code/R Shiny app.R
@@ -16,15 +16,12 @@ library(glue)
 
 # Assigning Input Vectors for the filter Selection (i might need to update this to make it interactive)
 {
-country <- c("burundi", "colombia")
+country <- c("Burundi", "Colombia")
 dataframe <- c("United Nations Multi-Partner Trust Fund (MPTF) documents",
-               "Organization for Economic Co-operation and Development Creditor Reporting System (OECD CRS)",
-               "International Aid Transparency Initiative (IATI)",
-               "crs",
-               "mptf")
+               "Organization for Economic Co-operation and Development Creditor Reporting System (OECD CRS)")
 sender_org_type <- unique(master_data$sender_orgtype)
 receiver_org_type <- unique(master_data$receiver_orgtype)
-#sector <- unique(Burundi_CRS_all_years$sector)
+sector <- unique(master_data$sector)
 relationships <- c("Number of Contracts", "Cost")
 }
 
@@ -40,16 +37,16 @@ ui <- { navbarPage("Research on International Policy Implementation Lab",
                             fluidRow(
                               column(4,
                                      
-                                     h4("Filter Based on Data"),
+                                     h4("Filter Based on Data", style = "text-decoration: underline;"),
                                      checkboxInput("data_filters", "Customize network based on data?"),
                                      conditionalPanel(
                                        condition = "input.data_filters == true",
                                        selectInput("select_dataframe", "What dataframe are you interested in?", choices= dataframe, selected = "mptf" ),
                                        selectInput("select_country", "What country are you interested in?", choices= country),
-                                       sliderInput("years", "What time year are you interested in?", value = 2007, min = 2005, max = 2021)
+                                       sliderInput("years", "What year are you interested in?", value = 2007, min = 2005, max = 2021, sep = "")
                                      ),
                                      
-                                     h4("Filter Based on Nodes"),
+                                     h4("Filter Based on Nodes", style = "text-decoration: underline;"),
                                      checkboxInput("node_filters", "Customize network based on nodes?"),
                                      conditionalPanel(
                                        condition = "input.node_filters == true",
@@ -57,20 +54,20 @@ ui <- { navbarPage("Research on International Policy Implementation Lab",
                                        selectInput("select_receiver_org_type", "What type of receiver organizations are you interested in?", choices= receiver_org_type, multiple = TRUE, selected = receiver_org_type)
                                      ),
                                      
-                                     h4("Filter Based on Relationships"),
+                                     h4("Filter Based on Relationships", style = "text-decoration: underline;"),
                                      checkboxInput("edge_filters", "Cutomize network based on edges?"),
                                      conditionalPanel(
                                        condition = "input.edge_filters == true",
                                        selectInput("select_relationship", "What type of relationship are you interested in?", choices= relationships),
-                                       sliderInput("num_contracts", "Filter relationships based on number of contracts.", value = c(1,20), min = 1, max = 20)
-                                       #selectInput("select_sector", "What sectors are you interested in?", choices= sector, multiple = TRUE),
+                                       sliderInput("num_contracts", "Filter relationships based on number of contracts.", value = c(1,20), min = 1, max = 20),
+                                       selectInput("select_sector", "What sectors are you interested in?", choices= sector, multiple = TRUE, selected = sector),
                                      )
                                      ),
                               column(8,
                                      titlePanel("Network Visualization of Peace Operations"),
                                      div(
                                        style = "border: 2px solid #444; border-radius: 10px;",
-                                       visNetworkOutput("network_viz", height = "85vh", width = "100%")
+                                       visNetworkOutput("network_viz", height = "75vh", width = "100%")
                                      ))
                               ),
                             fluidRow(
@@ -131,13 +128,15 @@ server <- function(input, output) {
   edges <- reactive({ 
     master_data %>%
       mutate(from = sender, to = receiver) %>%
-      select(from, to) %>%
+      select(from, to, sector, cost) %>%
       group_by(from, to) %>%
       mutate(value = n())%>%
       ungroup() %>%
       distinct(from, to, value, .keep_all = TRUE) %>%
-      filter(value >= input$num_contracts[1], value <= input$num_contracts[2]) %>%
-      mutate(title = glue("This relationship represents {value} contracts <br> from {from} <br> to {to}."))
+      filter(value >= input$num_contracts[1], value <= input$num_contracts[2],
+             sector %in% input$select_sector) %>%
+      mutate(title = glue("This relationship represents {value} contracts <br> from {from} <br> to {to} <br> within the {sector} sector.")) %>%
+      select(-sector)
     })
   
   output$edges_dataframe <- renderPrint(head(edges()))
@@ -158,7 +157,7 @@ server <- function(input, output) {
   
   ## Data Frame on display
   output$data_table <- renderDT({
-    datatable(display_data(), options = list(pageLength = 20))
+    datatable(display_data(), options = list(pageLength = 10))
   })
   
 }
